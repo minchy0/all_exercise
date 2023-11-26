cb7c53585c06fcb43dbd79c84319d4defb0b6a7f
minchy0 <148981757+minchy0@users.noreply.github.com>
2023-11-25 21:11:26 -0500

    Add files via upload

---
+++ car_oo_challenge.js
@@ -0,0 +1,57 @@
+class Vehicle {
+    constructor(make,model,year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk(){
+        return "Beep."
+    }
+    toString(){
+        return `This Vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
+
+let myFirstVehicle = new Vehicle("Honda", "Monster Truck", 1999);
+
+class Car extends Vehicle {
+    constructor(make,model,year){
+        super(make,model,year);
+        this.numWheels = 4;
+}
+}
+
+let myFirstCar = new Car("Toyota", "Corolla", 2005);
+
+class Motorcycle extends Vehicle {
+    constructor(make,model,year){
+        super(make,model,year);
+        this.numWheels = 2;
+    }
+    revEngine(){
+        return "VROOM!!!"
+    }
+}
+
+let myFirstMotorcycle = new Motorcycle("Honda", "Nighthawk", 2000);
+
+class Garage {
+    constructor(capacity){
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+    add(a){
+        if (!(a instanceof Car) && !(a instanceof Motorcycle)) {
+            return "Only vehicles are allowed in here!"
+        }
+        if (this.vehicles.length >= this.capacity){
+            return "Sorry, we're full."            
+        }
+        this.vehicles.push(a);
+        return "Vehicle added!";
+    }
+    
+}
+
+let garage = new Garage(2);
+
